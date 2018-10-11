47c2c2bd1fef80f74cb7099a075753bef8561b0b
JONES.JACOB <2200361@fbisd.flourbluffschools.net>
2018-10-11 14:12:30 -0500

    EoC

--- board.py
+++ board.py
@@ -1,12 +1,30 @@
 
 
-
-
-
 class Board:
+    
 
 
-def __init__(self):
+    def __init__(self):
+        self.board = board
+        self.width = width
+        self.hieght = hieght
+        
+        
+    def place_piece(self,pice,column):
+        pass
+        
+        
+    def empty_board(self):
+        pass
+    
+    
+    
+    def disp_board():
+        
+        
+        
+        
+        
     
     
     
