01ed63394eda41b594492e63e1950cd4668e0c76
HUGHES.ETHAN <2210321@fbisd.flourbluffschools.net>
2018-10-11 14:00:13 -0500

    another commit

---
+++ game.py
@@ -0,0 +1,20 @@
+from board import Board
+from player import Player
+
+class Game:
+    def __init__(self):
+        self.players = players
+        self.board = board
+        
+    def play_game(self):
+        pass
+    
+    def get_player_choice(self):
+        pass
+    
+class 
+        
+        
+    
+if __name__== "__main__":
+    pass
