a5a85f8ef97383eba0ef7dfd6a55bd30f403d39c
JONES.JACOB <2200361@fbisd.flourbluffschools.net>
2018-10-12 14:12:09 -0500

    slow progress

--- board.py
+++ board.py
@@ -4,27 +4,46 @@ class Board:
     
 
 
-    def __init__(self):
-        self.board = board
+    def __init__(self,width,height):
         self.width = width
-        self.hieght = hieght
+        self.height = height
+        self.board = [[' ']*self.width for i in range(self.height)]
         
         
-    def place_piece(self,pice,column):
+        
+        
+    def add_piece(self,column,piece):
         pass
         
         
     def empty_board(self):
+        print(self.board)
+    
+            
+    
+    def disp_board(self):
+        for i in range(self.width):
+            for j in range (self.height):
+                print(tdl[i][j], end=' ')
+            print()
+    
+    def is_full(self):
         pass
+        
+        
+def main():
+    b=Board(5,5)
+    b.disp_board()
+
+if __name__ == "__main__":
+    main()
     
     
     
-    def disp_board():
-        
-        
-        
-        
-        
+    
+    
+    
+    
     
     
     
