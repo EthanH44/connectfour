b1f23935f721e60110d527b02925bb2841df6606
HUGHES.ETHAN <2210321@fbisd.flourbluffschools.net>
2018-10-12 14:12:52 -0500

    wop

---
+++ player.py
@@ -0,0 +1,17 @@
+class Player:
+    def __init__(self):
+        self.name = name
+        self.choice = choice
+        self.turn = 0
+        
+    def get_name(self):
+        name1 = input("What is the name of your player: ")
+        
+        
+    
+    def get_choice(Board):
+        pass
+            
+        
+    
+    
