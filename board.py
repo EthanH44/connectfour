62f20382c24076a1263934fef2926b119be45275
JONES.JACOB <2200361@fbisd.flourbluffschools.net>
2018-10-15 14:12:07 -0500

    multiple problems worked on

--- board.py
+++ board.py
@@ -7,14 +7,18 @@ class Board:
     def __init__(self,width,height):
         self.width = width
         self.height = height
-        self.board = [[' ']*self.width for i in range(self.height)]
+        self.board = [['1']*self.width for i in range(self.height)]
         
         
         
         
-    def add_piece(self,column,piece):
+    def add_piece(self,col,piece):
+        for i in range(self.width):
+            col-1 = piece
+            
+        
+                                                         
         pass
-        
         
     def empty_board(self):
         print(self.board)
@@ -24,16 +28,26 @@ class Board:
     def disp_board(self):
         for i in range(self.width):
             for j in range (self.height):
-                print(tdl[i][j], end=' ')
+                print(self.board[i][j], end=' ')
             print()
     
     def is_full(self):
+        for i in range(self.width):
+            for j in range (self.height):
+                if self.board[i][j] == ' ':
+                    break
+        else:
+            return "Board is full. Tie!"
+                    
+                    
+    def check_winner(self):
         pass
         
         
 def main():
     b=Board(5,5)
     b.disp_board()
+    b.add_piece
 
 if __name__ == "__main__":
     main()
