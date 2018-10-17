dd33a69063c3da2669c0ba3c40e1cafd3d548b04
HUGHES.ETHAN <2210321@fbisd.flourbluffschools.net>
2018-10-17 14:10:30 -0500

    added most of play_game just waiting on check_winner from partner

--- game.py
+++ game.py
@@ -2,19 +2,39 @@ from board import Board
 from player import Player
 
 class Game:
-    def __init__(self,players,board):
-        self.players = players
-        self.board = board
+    def __init__(self,players,board,turn):
+        self.players = []
+        self.board = Board()
+        self.turn = 0
         
     def play_game(self):
-        pass
+        p1 = Player().get_name()
+        self.players.append(p1)
+        p2 = Player().get_name()
+        self.players.append(p2)
+        Board().disp_board()
+        while True:
+            try:
+                Player().get_choice(name)
+                Board().add_piece(choice)
+                Board().disp_board()
+                Board().is_full()
+                Board().check_winner()
+                
+            
+                
+            
+            
+            
     
     
     
 def main():
-    pass
+    play_game()
+
+
         
     
 if __name__== "__main__":
     main()
-    pass
+    
--- player.py
+++ player.py
@@ -1,20 +1,18 @@
-from board import Board
+
 
 class Player:
-    def __init__(self,piece):
-        self.name = self.get_name()
-        self.piece = piece
+    def __init__(self,name):
+        self.name = 'default'
         
     def get_name(self):
         name = input("What is the name of your player: ")
-    
-    def get_choice(self, board):
-        choice = input("What column would you like to place your piece: ")
-        return choice
+        
+    def get_choice(self,name):
+        choice = input(f"{name} pick a column")
+        turn = (turn + 1) % 2
     
 def main():
-    E = Player()
-    E.get_name()
- 
+    pass
+
 if __name__== "__main__":
     main()
