c74ec76617a27b670e89431020a05cd0f097fdbc
Chad Purdy <cpurdy@flourbluffschools.net>
2018-10-24 11:22:29 -0500

    feedback

--- board.py
+++ board.py
@@ -26,7 +26,7 @@ class Board:
     
             
     
-    def disp_board(self):
+    def disp_board(self): # crashes when a non-square board is used
         c=[]
         for x in range(self.width):
             c.append(x+1)
@@ -40,7 +40,7 @@ class Board:
                 print(self.board[i][j], end=' ')
             print()
     
-    def is_full(self):
+    def is_full(self): # should return True or False
         for i in range(self.width):
             for j in range (self.height):
                 if self.board[i][j] == '1':
@@ -49,7 +49,7 @@ class Board:
                 raise ValueError("Board is full. Tie!")
                     
                     
-    def check_winner(self):
+    def check_winner(self): # method is supposed to be check_win()
         for i in range(self.height):
             for j in range(self.width-3):
                 if self.board[i][j] == self.board[i][j+1]:
@@ -74,7 +74,7 @@ class Board:
             
         
 def main():
-    b=Board(5,5)
+    b=Board(6,7)
     
     b.disp_board()
     print(b.check_winner())
